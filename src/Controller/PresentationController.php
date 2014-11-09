5c5c5e886191f4451d06973cf46b687ce5d869b8
Mark Ferree <markferree@gmail.com>
2014-11-08 21:09:45 -0800

    Updating page callback to use config value

--- src/Controller/PresentationController.php
+++ src/Controller/PresentationController.php
@@ -9,8 +9,9 @@ use Drupal\Core\Controller\ControllerBase;
 
 class PresentationController extends ControllerBase {
   public function myPage() {
+    $seconds_to_wait = \Drupal::config('presentation.settings')->get('seconds_to_wait');
     $element = array(
-      '#markup' => 'Hello, world',
+      '#markup' => $seconds_to_wait,
     );
     return $element;
   }
